7692fc26ed1c4154ef3ade9bd3d485a2ad5f2b82
rjcalow <47124090+rjcalow@users.noreply.github.com>
2025-04-16 17:10:31 +0100

    start script

---
+++ start_telebots.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+scripts=(
+  'holly.py'
+)
+
+dir_="/home/holly/holly-script-collection/telebots/"
+python_env="/home/holly/holly_env/env/bin/python3"
+
+# Kill all running instances of the scripts at the start
+echo "Stopping any existing instances of the scripts..."
+for s in "${scripts[@]}"; do
+  pids=$(pgrep -f "$s")
+  if [ -n "$pids" ]; then
+    echo "Killing processes for $s (PIDs: $pids)"
+    kill "$pids"
+  fi
+done
+echo "All existing instances stopped."
+sleep 5 # Give some time for processes to terminate
+
+# Main loop
+while true; do
+  for s in "${scripts[@]}"; do
+    process_id=$(pgrep -f "$s")
+
+    if [ -n "$process_id" ]; then
+      echo "$s is running"
+    else
+      echo "$s is not running, restarting..."
+      nohup "$python_env" "$dir_"$s "command" "arg" &
+      sleep 1
+    fi
+  done
+  sleep 120 # Run every 2 minutes
+done
